e28b0bcbe28c88871f31fb9db38a2c8eed9b9f66
Ted Cai <ted.xmdgj@gmail.com>
2017-10-10 17:29:58 +0800

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < BaseController
+  before_action :set_user, only: [:show]
+
   def index
     @users = User.all
+  end
+  
+  def show
+    @membership_records = @user&.membership_records || []
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
--- app/models/concerns/timer.rb
+++ app/models/concerns/timer.rb
@@ -5,4 +5,8 @@ module Timer
     self.created_at = Time.now
     self.updated_at = Time.now
   end
+
+  def set_record_time
+    self.record_time = Time.now
+  end
 end
