c90efb6a35ddbd8a146518d711bf5a65346ff3a6
Ted Cai <ted.xmdgj@gmail.com>
2017-10-13 11:55:59 +0800

    enable to create new records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < BaseController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :new_record, :create_new_record]
   before_action :authenticate_user!
 
   def index
@@ -16,6 +16,26 @@ class UsersController < BaseController
         @score_list[record.id] = initial_score
       end
     end
+  end
+
+  def new_record
+    redirect_to user_path(@user.id) if @user.id != current_user.id
+  end
+
+  def create_new_record
+    year, month, day = params[:year], params[:month], params[:day]
+    daily_10m, daily_18m, basic_score = params[:daily_10m], params[:daily_18m], params[:basic_score]
+    time = Time.new(year, month, day)
+    records = @user.membership_records.where(record_time: time)
+    if @user.id == curent_user.id && records.blank?
+      membership_record = @user.membership_records.create
+      membership_record.update(record_time: time)
+      [daily_10m, daily_18m, basic_score].each do |policy_id|
+        membership_record.membership_record_details.create(membership_score_policy_id: policy_id) unless policy_id == "0"
+      end
+    end
+
+    redirect_to user_path(@user.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show) do
+    member do
+      get :new_record
+      post :create_new_record
+    end
+  end
 
   root to: "users#index"
 end
